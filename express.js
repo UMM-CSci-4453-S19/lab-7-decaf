cfc996ee2e15fe3ff8300aff1aed136d11abcf85
schon094 <schon094@morris.umn.edu>
2019-03-02 14:48:44 -0600

    finish angular js tutorial

--- express.js
+++ express.js
@@ -3,4 +3,8 @@ app = express(),
 port = process.env.PORT || 1337;
 
 app.use(express.static(__dirname + '/public'));
+app.get("/buttons",function(req,res){
+  res.send("Hello World! May I interest you in some... <em>butoons</em>?");
+});
+
 app.listen(port);
