bac1e0a846482c4ed59997d13e3ae8db2dfa22da
monjo003 <33473534+monjo003@users.noreply.github.com>
2019-03-06 15:12:50 -0600

    complete lab7server.js
    now queries and returns json table

--- first_buttons/lab7server.js
+++ first_buttons/lab7server.js
@@ -3,13 +3,17 @@ var mysql=require("mysql");
 credentials.host="ids";
 var pool=mysql.createPool(credentials);
 
-var get_till = pool.getConnection(function(err,conn){
+pool.getConnection(function(err,conn){
   if(!err){
       conn.query('Select * from jafi.till_buttons',function(err,rows,fields){
           if(err){
             console.log('Error looking up databases');
           } else {
-            console.log(rows);
+            for(row in rows) {
+                console.log(rows[row]);
+                buttons.push(rows[row]);
+            }
+
           }
           conn.release()
           pool.end()
@@ -17,6 +21,7 @@ var get_till = pool.getConnection(function(err,conn){
   } else{
       console.log('Error making connection')
   }
+
 });
 
 
@@ -24,14 +29,11 @@ var express=require('express'),
 app = express(),
 port = process.env.PORT || 1337;
 
-var buttons=[{"buttonID":1,"left":10,"top":70,"width":100,"label":"hotdogs","invID":1},
-{"buttonID":2,"left":110,"top":70,"width":100,"label":"hambugers","invID":2},
-{"buttonID":3,"left":210,"top":70,"width":100,"label":"bannanas","invID":3},
-{"buttonID":4,"left":10,"top":120,"width":100,"label":"milkduds","invID":4}]; //static buttons
+var buttons= [];
 
 app.use(express.static(__dirname + '/public')); //Serves the web pages
 app.get("/buttons",function(req,res){ // handles the /buttons API
-  res.send(get_till);
+  res.send(buttons);
 });
 
 app.listen(port);
